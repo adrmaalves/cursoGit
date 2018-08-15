1096c47b1d308b4ea3303067d44867433de0d84c
Adriano Alves <adrianomacedo_07@hotmail.com>
2018-08-15 23:05:09 +0100

    Projecto inciado mostrando mensagem ao utilizador

---
+++ Curso_Projecto18/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Projecto18
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Projecto criado para servir de exemplo ao estudo do Git e GitHub!");
+			Console.ReadLine();
+		}
+	}
+}
